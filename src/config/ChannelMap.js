96b666a5c6206b054797ab7eb6de6ddbb4a7526c
flexisdanish-cloud <flexisdanish@gmail.com>
2026-07-21 23:01:28 +0530

    Update ChannelMap.js

--- src/config/ChannelMap.js
+++ src/config/ChannelMap.js
@@ -1,5 +1,5 @@
 export const channelMap = {
-  buy49: { channelId: "-1003896827128", amount: 49 },
-  buy99: { channelId: "-1004365642583", amount: 99 },
-  buy2499: { channelId: "-1004491915278", amount: 2499 }
+  buy49: { channelId: "-1004399598839", amount: 49 },
+  buy99: { channelId: "-1004332757409", amount: 99 },
+  buy2499: { channelId: "-1004368764676", amount: 2499 }
 };
